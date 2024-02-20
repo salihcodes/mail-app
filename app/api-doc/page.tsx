fc76e1de334398fc962a662af187dd4ce803618d
salihcodes <salih.codes@proton.me>
2024-02-21 00:11:18 +0200

    fix: moved react-swagger component

--- app/api-doc/page.tsx
+++ app/api-doc/page.tsx
@@ -1,5 +1,5 @@
 import { getApiDocs } from "@/lib/swagger";
-import ReactSwagger from "./react-swagger";
+import ReactSwagger from "./components/react-swagger";
 
 export default async function IndexPage() {
   const spec = await getApiDocs();
