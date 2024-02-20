89b8396f2ba1867e7018ac5eed3552f138269e9e
salihcodes <salih.codes@proton.me>
2024-02-21 00:16:42 +0200

    added error handling to api docs page

--- app/api-doc/page.tsx
+++ app/api-doc/page.tsx
@@ -1,8 +1,12 @@
 import { getApiDocs } from "@/lib/swagger";
 import ReactSwagger from "./components/react-swagger";
 
-export default async function IndexPage() {
+export default async function ApiDocPage() {
   const spec = await getApiDocs();
+  if (!spec) {
+    return <div>Api docs not found</div>;
+  }
+
   return (
     <section className="container">
       <ReactSwagger spec={spec} />
